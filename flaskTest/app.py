c1e769c62cd4399a60e89fc2a1cd3b21e2b2ccb8
quantum35 <quantumcomputing35@aol.com>
2018-04-24 19:54:44 +0300

    Add Test  For Admin to Modify mealOptions,View Orders made,Check the Profit of the day and add test  for customer to view Orders and make Orders

--- flaskTest/app.py
+++ flaskTest/app.py
@@ -20,9 +20,94 @@ class register(Resource):
                 'email':'quantum@gmail.com',
                 'password':'password'
                 },201
+#Admin
+class addNewMeal(Resource):
+    def post(self):
+        data = request.get_json()
+        meal_id = data['meal_id']
+        name = data['name']
+        price = data['price']
+        return {'meal_id':'meal1',
+                'name':'Chips',
+                'price':'1000'
+                },201
+class modifyMeal(Resource):
+    def put(self):
+        data = request.get_json()
+        meal_id = data['meal_id']
+        name = data['name']
+        price = data['price']
+        return {
+                'meal_id':'1',
+                'name':'Ugali',
+                'price':'500'
+                },201
+class deleteMealOption(Resource):
+    def delete(self):
+        data = request.get_json()
+        meal_id = data['meal_id']
+        name = data['name']
+        price = data['price']
+        return {'meal_id':'meal1',
+                'name':'Chips',
+                'price':'1000'
+                },201
+        
+
+class setUpmealDay(Resource):
+     def post(self):
+        data = request.get_json()
+        meal_id = data['id']
+        name = data['mname']
+        price = data['mprice']
+        return {'meal_id':'meal1',
+                'name':'Chips',
+                'price':'1000'
+                },201
+class checkOrdersMadebyUser(Resource):
+    def get(self):
+        return {'message':'Here are The Orders Made by users'}
+
+class checkTodaysProfit(Resource):
+    def get(self):
+        return {'message':'Todays Profit is'}
+class checkOrderHistoryAdmm(Resource):
+    def get(self):
+        return {'message':'Here is Your History'}
+class checkLogedinAdmins(Resource):
+    def get(self):
+        return {'message':'Return More Than one Admin Loged In'}
+#Customers
+class custCheckMenu(Resource):
+    def get(self):
+        return{'message':'Here is your Menu Of the Day'}
+    def post(self):
+        data = request.get_json()
+        name = data['name']
+        price = data['price']
+        return {'name':'ugali',
+                'price':'1000'
+                },201
+class checkOrderHistory(Resource):
+    def get(self):
+        return{'message':'Here is your Order History'}
+
+class getNotification(Resource):
+    def get(self):
+        return{'message':'You have one New Message'}
+
 
 api.add_resource(login,'/')
 api.add_resource(register,'/register')
-
+api.add_resource(deleteMealOption,'/detemeal')
+api.add_resource(addNewMeal, '/addNewMeal')
+api.add_resource(modifyMeal,'/modifyMeal')
+api.add_resource(custCheckMenu,'/custCheckMenu')
+api.add_resource(checkOrdersMadebyUser,'/checkOrdersMadebyUser')
+api.add_resource(checkTodaysProfit,'/checkTodaysProfit')
+api.add_resource(checkOrderHistory,'/checkOrderHistory')
+api.add_resource(getNotification,'/getNotification')
+api.add_resource(checkOrderHistoryAdmm,'/checkOrderHistoryAdmm')
+api.add_resource(checkLogedinAdmins,'/checkLogedinAdmins')
 if __name__ == '__main__':
     app.run(port=5000, debug=True)    
--- flaskTest/test_app.py
+++ flaskTest/test_app.py
@@ -8,7 +8,11 @@ class TestFlaskApp(unittest.TestCase):
         app.testing = True
         self.app = app.test_client()
         self.data = {"username":"quantum", "email":"quantum@gmail.com","password":"password"}
-
+        self.data = {"username":"quantum", "email":"quantum@gmail.com","password":"password"}
+        self.data1 = {"meal_id":"meal1", "name":"Chips","price":"1000"}
+        self.data2 = {"meal_id":"meal1", "name":"Ugali","price":"500"}
+        self.data3 = {"meal_id":"meal1", "name":"Chips","price":"1000"}
+        self.data4 = {"name":"ugali", "price":"1000"}
 
     def test_login(self):
         response = self.app.get('/')
@@ -23,8 +27,80 @@ class TestFlaskApp(unittest.TestCase):
         self.assertEqual(result["email"], "quantum@gmail.com")
         self.assertEqual(result["password"], "password")
         self.assertEqual(response.status_code, 201)
+    
+    def test_addNewMeal(self):
+        response = self.app.post('/addNewMeal', data = json.dumps(self.data2) , content_type = 'application/json')
+        result = json.loads(response.data)
+        self.assertEqual(result["meal_id"], "meal1")
+        self.assertEqual(result["name"], "Chips")
+        self.assertEqual(result["price"], "1000")
+        self.assertEqual(response.status_code, 201)
 
+    def test_modifyMeal(self):
+        response = self.app.post('/modifyMeal', data = json.dumps(self.data1) , content_type = 'application/json')
+        result = json.loads(response.data)
+        self.assertEqual(result["meal_id"], "meal1")
+        self.assertEqual(result["name"], "Ugali")
+        self.assertEqual(result["price"], "500")
+        self.assertEqual(response.status_code, 201)
 
+    def test_deleteMealOption(self):
+        response = self.app.delete('/detemeal',data=json.dumps(self.data1,content_type = 'application/json' ))
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Meal Successfuly Deleted")
+        self.assertEqual(response.status_code, 200)
+
+    def test_MealOfTheDay(self):
+        response = self.app.post('/modifyMeal', data = json.dumps(self.data1) , content_type = 'application/json')
+        result = json.loads(response.data)
+        self.assertEqual(result["meal_id"], "meal1")
+        self.assertEqual(result["name"], "Chips")
+        self.assertEqual(result["price"], "1000")
+        self.assertEqual(response.status_code, 201)
+    
+    def test_OrdersMadeByUsers(self):
+        response = self.app.get('/checkOrdersMadebyUser')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Here are The Orders Made by users")
+        self.assertEqual(response.status_code, 200)
+    def test_profitMade(self):
+        response = self.app.get('/checkTodaysProfit')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Todays Profit is")
+        self.assertEqual(response.status_code, 200)
+    def test_admCheckOrderHistory(self):
+        response = self.app.get('/checkOrderHistoryAdmm')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Here is Your History")
+        self.assertEqual(response.status_code, 200)
+    def test_noAdmin(self):
+        response = self.app.get('/checkLogedinAdmins')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Return More Than one Admin Loged In")
+        self.assertEqual(response.status_code, 200)
+
+#Cutomers
+    def test_custCheckDaysMenu(self):
+        response = self.app.get('/custCheckMenu')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Here is your Menu Of the Day")
+        self.assertEqual(response.status_code, 200)
+    def test_changeMealChoice(self):
+        response = self.app.post('/modifyMeal', data = json.dumps(self.data4) , content_type = 'application/json')
+        result = json.loads(response.data)
+        self.assertEqual(result["name"], "ugali")
+        self.assertEqual(result["price"], "1000")
+        self.assertEqual(response.status_code, 201)
+    def test_checkOrderHistory(self):
+        response = self.app.get('/checkOrderHistory')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "Here is your Order History")
+        self.assertEqual(response.status_code, 200)
+    def test_getNotification(self):
+        response = self.app.get('/getNotification')
+        result = json.loads(response.data)
+        self.assertEqual(result["message"], "You have one New Message")
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
